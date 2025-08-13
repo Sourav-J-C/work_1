3be82a4ff8281ff3eeb04b085bd6d8ec601f5ef2
sourav-jc <sourav@gho.care>
2025-08-13 15:10:14 +0530

    work change

--- lib/controller/message_controller.dart
+++ lib/controller/message_controller.dart
@@ -1,3 +1,7 @@
 import 'package:get/get.dart';
 
-class MessageController extends GetxController {}
+class MessageController extends GetxController {
+  var isSmschecked = false.obs;
+  var isEmailchecked = false.obs;
+  var isWhatsappchecked = false.obs;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:work_1/view/message_scrn.dart';
 
 void main() {
